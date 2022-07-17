170d7d2aadac2ecf198cf1d349815521e83067c2
SoufianeVault <105563785+SoufianeVault@users.noreply.github.com>
2022-07-17 23:23:39 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Formm from './components/Formm';
+
+function App() {
+      <h1>Form</h1>
+
+  return (
+    <div>    <h1 class="text-danger p-3 mb-2 bg-light">Form</h1>
+
+       <Formm />
+     </div>
+ 
+  );
+}
+
+export default App;
